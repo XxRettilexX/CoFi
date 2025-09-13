54fdb890aebf15be5ac0cc075ea2511615919f71
Vito <80618628+XxRettilexX@users.noreply.github.com>
2025-09-13 11:52:56 +0200

    modified:   app/Http/Middleware/AdminMiddleware.php

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,14 +4,16 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->check() && auth()->user()->role === 'admin') {
+        if (Auth::check() && Auth::user()->role === 'admin') {
             return $next($request);
         }
+
 
         abort(403, 'Accesso negato.');
     }
